9aae5a3ed74828350562dbf934e22b9997816c0b
Jani Harju <e2302304@edu.vamk.fi>
2024-11-05 11:35:32 +0200

    Lisätty konsulttidata ja näyttö ConsultantList-komponentissa

--- src/components/ConsultantList.js
+++ src/components/ConsultantList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import consultants from '../data';
 
 function ConsultantList() {
   return (
     <div>
       <h2>Konsulttilistaus</h2>
-      <p>Tässä näkyy lista konsultteja.</p>
+      <ul>
+        {consultants.map((consultant) => (
+          <li key={consultant.id}>
+            <strong>{consultant.name}</strong> - Osaaminen: {consultant.expertise} - Kokemus: {consultant.experience} vuotta
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,9 @@
+
+const consultants = [
+    { id: 1, name: "Matti Meikäläinen", expertise: "React, Node.js", experience: 5 },
+    { id: 2, name: "Anna Esimerkki", expertise: "Angular, Python", experience: 3 },
+    { id: 3, name: "Teemu Testaaja", expertise: "Vue, Java", experience: 7 }
+  ];
+  
+  export default consultants;
+  
